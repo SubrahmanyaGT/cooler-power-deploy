60e6b639c88be97db46c0f5368304096aca5045f
SubrahmanyaGT <95739489+SubrahmanyaGT@users.noreply.github.com>
2022-05-12 19:06:31 +0530

    Update shopify.js

--- pages/api/shopify.js
+++ pages/api/shopify.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
         }
       }
       `)
-    res.status(200).json(  {Porducts:Porducts} )
+    res.status(200).json(  {Porducts:check} )
 // }
   }
   
